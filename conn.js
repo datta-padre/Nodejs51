7eef3d2115554cdc9e815a5565cfb43b3b684525
Datta Padre <datta.padre@gmail.com>
2026-04-10 17:40:21 +0530

    Lunch1

--- conn.js
+++ conn.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const util = require("util");
 
 const conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"a2z51project"
+    host:"byntlnhm1gumhdshoqwc-mysql.services.clever-cloud.com",
+    user:"uepwrrc5m4hpuokl",
+    password:"9DvWW7HDQExLk6uI1Bbe",
+    database:"byntlnhm1gumhdshoqwc"
 })
 
 const exe = util.promisify(conn.query).bind(conn);
